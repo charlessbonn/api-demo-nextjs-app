2b66b4c39675e49a0e92640be59e34c3926ca67e
Charles Bon Chua <chuacharlesbon@gmail.com>
2025-05-21 16:55:36 +0800

    Updated: User Session

--- lib/utils/cors.ts
+++ lib/utils/cors.ts
@@ -1,8 +1,10 @@
 // --- Helper: Generate CORS headers
 
+import { MyConfig } from "./config";
+
 export function corsHeaders(req: Request): HeadersInit {
     const origin = req.headers.get('origin') || "";
-    const allowedOrigins = ['http://localhost:4200'];
+    const allowedOrigins = [MyConfig.devMode ? (MyConfig.devWebUrl ?? "http://localhost:3000") : (MyConfig.prodWebUrl ?? "http://localhost:3000")];
     const isAllowed = allowedOrigins.includes(origin);
 
     return {
--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import { MyConfig } from "./lib/utils/config";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -9,7 +10,7 @@ const nextConfig: NextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:4200" },
+          { key: "Access-Control-Allow-Origin", value: MyConfig.devMode ? (MyConfig.devWebUrl ?? "http://localhost:3000") : (MyConfig.prodWebUrl ?? "http://localhost:3000") },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ],
